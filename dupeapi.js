277dadf00f2456df4a46bd1ceb7eef12ea55ebd9
Pierre Constantineau <jpconstantineau@gmail.com>
2023-04-11 16:36:16 -0600

    added

--- dupeapi.js
+++ dupeapi.js
@@ -173,8 +173,7 @@ app.get("/agent/:agentid", async (req, res)  =>  {
     throw err;
   } finally {
   if (conn) return conn.end();
-
-  res.json(response);
+  res.status(200).json(response);
   }
 });
 
